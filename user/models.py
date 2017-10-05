3a438311b7126117442dcdb177dfaca8d9109cc4
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-10-05 22:52:16 +0200

    fixed reverse cursor conflicts and created database migrations

--- user/models.py
+++ user/models.py
@@ -6,17 +6,16 @@ class Language(models.Model):
     """
     Model to store language data
     """
-    name = models.CharField(45)
-    region = models.CharField(45, blank=True)
+    name = models.CharField(max_length=45)
+    region = models.CharField(max_length=45, blank=True)
 
 
 class UserProfile(models.Model):
     """
     Model to store details about the users
     """
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     age = models.IntegerField(null=False)
-    first_language = models.ForeignKey(Language, blank=False, null=True)
-    second_language = models.ForeignKey(Language, blank=True)
-    third_language = models.ForeignKey(Language, blank=True)
-
+    first_language = models.ForeignKey(Language, blank=False, related_name='first_language')
+    second_language = models.ForeignKey(Language, blank=True, related_name='second_language')
+    third_language = models.ForeignKey(Language, blank=True, related_name='third_language')
