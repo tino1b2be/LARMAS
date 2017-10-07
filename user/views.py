891dc541222ee0605f7a650f1960da2832a95fd1
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-10-07 17:54:28 +0200

    Added place holder responses to the User and UserRegistration geta nd post methods.

--- user/views.py
+++ user/views.py
@@ -1,15 +1,30 @@
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.status import HTTP_403_FORBIDDEN
 
 
 class User(APIView):
     """
     View class to display or change user details
     """
-    pass
+    def get(self, request):
+        # todo implement
+        return Response({}, status=HTTP_403_FORBIDDEN)
+
+    def post(self, request):
+        # todo implement
+        return Response({}, status=HTTP_403_FORBIDDEN)
 
 
 class UserRegistration(APIView):
     """
     View class to register a new user
     """
-    pass
+
+    def get(self, request):
+        # todo implement
+        return Response({}, status=HTTP_403_FORBIDDEN)
+
+    def post(self, request):
+        # todo implement
+        return Response({}, status=HTTP_403_FORBIDDEN)
