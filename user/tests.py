3f7679302e8128d9f30e1b69c9002975054ae74b
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-10-08 02:06:04 +0200

    Created test stubs for registration post method

--- user/tests.py
+++ user/tests.py
@@ -5,6 +5,10 @@ from django.urls import reverse
 class UserRegistration(TestCase):
 
     def test_user_registration_get(self):
+        """
+        Test the user registration get method
+        :return: pass if 403 error
+        """
         response = self.client.get(reverse('user:create_user'))
         self.assertEquals(response.status_code, 403)
 
@@ -13,6 +17,34 @@ class UserRegistration(TestCase):
         # todo populate data object to register new user
         response = self.client.post(reverse('user:create_user'), data)
         self.assertEquals(response.status_code, 400)
+
+    # todo implement test
+    def test_no_username_field(self):
+        pass
+
+    def test_non_unique_username(self):
+        pass
+
+    def test_no_password_field(self):
+        pass
+
+    def test_no_first_language_field(self):
+        pass
+
+    def test_no_extra_languages(self):
+        pass
+
+    def test_second_language(self):
+        pass
+
+    def test_third_language(self):
+        pass
+
+    def test_wrong_first_language(self):
+        pass
+
+    def test_wrong_second_language(self):
+        pass
 
 
 class User(TestCase):
