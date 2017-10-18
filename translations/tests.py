5b4e560cd32a16b5d6a3ca9a70a921b9be3ddaff
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-10-18 18:28:19 +0200

    Added unimplemented tests

--- translations/tests.py
+++ translations/tests.py
@@ -5,48 +5,48 @@ class TestTranslationUpload(APITestCase):
 
     def test_upload_translation_correct(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_upload_translation_no_language(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_upload_translation_no_original_prompt(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_upload_translation_unknown_original_prompt(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_upload_translation_unsupported_language(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_upload_translation_no_text_field(self):
         # todo
-        self.fail()
+        self.fail("Not yet implemented")
 
 
 class TestTranslationDetails(APITestCase):
 
     def test_show_translations_all_not_allowed(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translations_all(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translation_by_id(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translation_wrong_id(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translations_parallel(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translations_parallel_unsupported_first(self):
-        self.fail()
+        self.fail("Not yet implemented")
 
     def test_show_translations_parallel_unsupported_second(self):
-        self.fail()
+        self.fail("Not yet implemented")
