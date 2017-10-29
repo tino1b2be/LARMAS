44f0cd027ce310311508a36e2e60aa6e695940f1
Tinotenda Chemvura <ttchemvura@gmail.com>
2017-10-29 16:50:50 +0200

    implemented endpoint for one time users.

--- annotations/views.py
+++ annotations/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.core.exceptions import SuspiciousFileOperation, ObjectDoesNotExist
 from rest_framework.generics import RetrieveAPIView, ListAPIView
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser, AllowAny
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, \
     HTTP_500_INTERNAL_SERVER_ERROR
@@ -11,35 +12,52 @@ from annotations.models import PromptRecording
 from prompts.models import Prompt, DistributedPrompt
 from annotations.serializers import \
     PromptRecordingSerializer
+from user.util import create_one_time_profile
 
 
 class PromptUploadView(APIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (AllowAny,)
 
     def post(self, request):
         data = {'detail': 'prompt field is required.'}
-        # check if all the relevant data is there
-        if not request.POST.__contains__('prompt'):
-            return Response(data, status=HTTP_400_BAD_REQUEST)
-        if not request.POST.__contains__('annotation'):
-            data['detail'] = 'annotation field is required.'
-            return Response(data, status=HTTP_400_BAD_REQUEST)
-        if not request.FILES.__contains__('file'):
-            data['detail'] = 'No file attached.'
-            return Response(data, status=HTTP_400_BAD_REQUEST)
-
         try:
-            user = request.user  # todo fix this
+            # check if all the relevant data is there
+            if not request.POST.__contains__('prompt'):
+                return Response(data, status=HTTP_400_BAD_REQUEST)
+            if not request.POST.__contains__('annotation'):
+                data['detail'] = 'annotation field is required.'
+                return Response(data, status=HTTP_400_BAD_REQUEST)
+            if not request.FILES.__contains__('file'):
+                data['detail'] = 'No file attached.'
+                return Response(data, status=HTTP_400_BAD_REQUEST)
+
+            # create a random user
+            temp_user = User()
+            if request.user.is_authenticated:
+                user = request.user
+            else:
+                try:
+                    user = create_one_time_profile(
+                        post=request.POST, temp_user=temp_user)
+                except ObjectDoesNotExist:
+                    data['detail'] = 'first_language field is required' \
+                                     ' for one time contributors.'
+                    return Response(data, status=HTTP_400_BAD_REQUEST)
+
             pk = request.POST.get('prompt')
             annotation = request.POST.get('annotation')
             try:
                 prompt = Prompt.objects.get(pk=pk)
             except ObjectDoesNotExist:
+                if not request.user.is_authenticated:
+                    temp_user.delete()
                 data['detail'] = 'This prompt does not exist.'
                 return Response(data, status=HTTP_400_BAD_REQUEST)
             file = request.FILES.get('file')
             if len(file.name) < 5:
                 # filename too short
+                if not request.user.is_authenticated:
+                    temp_user.delete()
                 raise SuspiciousFileOperation
 
             # todo validate file
@@ -51,15 +69,19 @@ class PromptUploadView(APIView):
                 file_type=file.name[-3:].upper(),
                 annotation=annotation,
             )
-            try:
-                d1 = DistributedPrompt.objects.get(prompt=prompt, user=user)
-            except ObjectDoesNotExist:
-                data['detail'] = 'This prompt was not given to you.'
-                return Response(data, status=HTTP_400_BAD_REQUEST)
+            if request.user.is_authenticated:
+                try:
+                    d1 = DistributedPrompt.objects.get(
+                        prompt=prompt,
+                        user=user,
+                    )
+                    d1.recorded = True
+                    d1.save()  # save distributed prompt object
+                except ObjectDoesNotExist:
+                    data['detail'] = 'This prompt was not given to you.'
+                    return Response(data, status=HTTP_400_BAD_REQUEST)
 
             prompt.number_of_recordings += 1
-            d1.recorded = True
-            d1.save()  # save distributed prompt object
             prompt.save()  # save prompt object
             recording.save()  # save recording
             s = PromptRecordingSerializer(recording)
